a9518dd01e5f826f5b2b827dcddf71b5803e2997
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-19 13:25:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const div = document.getElementById('line');
+
+div.style.tranform = "rotate("360")deg";
+
+function rotateDiv(){
+	div.style.tranform = `rotate(${div.style.tranform})deg`; 
+
+	requestAnimationFrame(rotateDiv);
+}
+
+rotateDiv();
